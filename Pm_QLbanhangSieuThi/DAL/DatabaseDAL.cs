daae25e893aacd9fa6d05583d92fcec28a965b70
Nguyen Truong <truong.op1998@gmail.com>
2019-12-27 02:05:49 +0700

    commit2

--- Pm_QLbanhangSieuThi/DAL/DatabaseDAL.cs
+++ Pm_QLbanhangSieuThi/DAL/DatabaseDAL.cs
@@ -5,35 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
-using System.Windows.Forms;
-
 using DTO;
 namespace DAL
 {
     public class DatabaseDAL
     {
-        private static SqlCommand command;
+        private static SqlCommand command = new SqlCommand();
         private static SqlConnection connection = new SqlConnection();
         private static string chuoiConnection = @"Data Source=DESKTOP-243RGDN\SQLEXPRESS;Initial Catalog=QL_BanHangSieuThi;Integrated Security=True";
-        private static string chuoiConnection2 = @"Data Source=DESKTOP-P35V73J\SQLEXPRESS;Initial Catalog=QL_BanHangSieuThi;Integrated Security=True";
+        //private static DataTable tb = new DataTable();
         private static SqlDataAdapter sqlAdap;
-        private static SqlTransaction sqlTransaction;
 
-        public static bool TestConnection()
-        {
-            if(connection.State == ConnectionState.Open)
-            {
-                return true;
-            }
-            return false;
-        }
 
-        public static void CreatConn()
+        public static void creatConn()
         {
             try
             {
-                connection.ConnectionString = chuoiConnection2;
-                connection.Open();
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = chuoiConnection;
+                    connection.Open();
+
+                }
             }
             catch (Exception ex)
             {
@@ -42,95 +35,54 @@ namespace DAL
             }
         }
 
-        public static void CloseConnection()
-        {
-            connection.Close();
-        }
-
-        public static void Transaction()
-        {
-            sqlTransaction = command.Connection.BeginTransaction();
-        }
-
-        public static void Commit()
-        {
-            sqlTransaction.Commit();
-        }
-
-        public static void Rollback()
-
-        {
-            sqlTransaction.Rollback();
-        }
 
 
-
-        public static int ExecuteNonQuery(string query, object[] parameter = null)
-        {
-
-            try
-            {
-                var data = 0;
-                if (!TestConnection())
-                {
-                    CreatConn();
-                }
-                command = new SqlCommand(query, connection);
-                command.Transaction = sqlTransaction;
-
-                if (parameter != null)
-                {
-                    string[] listPara = query.Split(' ');
-                    int i = 0;
-                    foreach (string item in listPara)
-                    {
-                        if (item.Contains('@'))
-                        {
-                            command.Parameters.AddWithValue(item, parameter[i]);
-                            i++;
-                        }
-                    }
-                }
-                data = command.ExecuteNonQuery();
-                return data;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                throw ex;
-            }
-        }
-
-        public static DataTable ReeadData(string query, object[] parameter = null)
+        public static int executeNonQuery(string query, params object[] arr)
         {
             try
             {
-
-                if (!TestConnection())
+                if (connection.State == 0)
                 {
-                    CreatConn();
+                    creatConn();
                 }
-                command = new SqlCommand();
+
                 command.Connection = connection;
+                command.CommandType = CommandType.Text;
                 command.CommandText = query;
-                command.Transaction = sqlTransaction;
-                if (parameter != null)
+                if (arr != null)
                 {
-                    string[] listPara = query.Split(' ');
-                    int i = 0;
-                    foreach (string item in listPara)
+                    string[] pra = query.Split('@');
+                    for (int i = 1; i < pra.Length; i++)
                     {
-                        if (item.Contains('@'))
+                        if (pra[i].Contains(","))
                         {
-                            command.Parameters.AddWithValue(item, parameter[i]);
-                            i++;
+                            pra[i] = pra[i].Split(',')[0];
+                        }
+                        else if (pra[i].Contains(")"))
+                        {
+                            pra[i] = pra[i].Split(')')[0];
+                        }
+                        else if (pra[i].Contains(" "))
+                        {
+                            pra[i] = pra[i].Split(' ')[0];
                         }
                     }
+
+
+                    int j = 1;
+                    foreach (object obj in arr)
+                    {
+                        command.Parameters.AddWithValue("@" + pra[j], obj);
+                        j++;
+                    }
+
                 }
-                DataTable tb = new DataTable();
-                sqlAdap = new SqlDataAdapter(command);
-                sqlAdap.Fill(tb);
-                return tb;
+                int x = command.ExecuteNonQuery();
+                command.Parameters.Clear();
+                connection.Close();
+                return x;
+
+
             }
             catch (Exception ex)
             {
@@ -140,17 +92,17 @@ namespace DAL
         }
 
 
-        public static DataTable ReeadDataToTable(string query, params object[] arr)
+        public static DataTable readDataToTable(string query, params object[] arr)
         {
             try
             {
 
-                if (!TestConnection())
+                if (connection.State == 0)
                 {
-                    CreatConn();
+                    creatConn();
                 }
-                command = new SqlCommand();
                 command.Connection = connection;
+                command.CommandType = CommandType.Text;
                 command.CommandText = query;
                 if (arr != null)
                 {
@@ -183,6 +135,8 @@ namespace DAL
                 DataTable tb = new DataTable();
                 sqlAdap = new SqlDataAdapter(command);
                 sqlAdap.Fill(tb);
+                command.Parameters.Clear();
+                connection.Close();
                 return tb;
             }
             catch (Exception ex)
@@ -193,11 +147,11 @@ namespace DAL
         }
 
 
-        public static int ExecuteNonQueryStoredProcedure(string query,params object[] arr)
+        public static int executeNonQueryStoredProcedure(string query, params object[] arr)
         {
             try
             {
-                if (!TestConnection())
+                if (connection.State == 0)
                 {
                     creatConn();
                 }
@@ -246,7 +200,7 @@ namespace DAL
         }
 
 
-        public static DataTable ReadDataToTableStoredProcedure(string query, params object[] arr)
+        public static DataTable readDataToTableStoredProcedure(string query, params object[] arr)
         {
             try
             {
@@ -287,6 +241,7 @@ namespace DAL
 
                 }
                 DataTable tb = new DataTable();
+
                 sqlAdap = new SqlDataAdapter(command);
                 sqlAdap.Fill(tb);
                 command.Parameters.Clear();
@@ -295,12 +250,11 @@ namespace DAL
             }
             catch (Exception ex)
             {
+
                 throw ex;
             }
         }
-
-       
     }
-       
+
 
 }
